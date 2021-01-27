0264d41b0dd5ed85c7dae1939a48ab895423a1e2
AyseCaTo <71521004+AyseCaTo@users.noreply.github.com>
2021-01-27 00:32:34 -0500

    22

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello world B21");
+        System.out.println("Hello world B22");
         //watching more movie
     }
 }
